e991dfc10bd0baa795966e8a204e192eef1ec89f
Shefan Darren <shefandarren@darren.local>
2022-06-29 17:27:10 +0530

    Content type and body mismatch final

--- src/main/java/backend/ContentTypeAndBodyMismatch.java
+++ src/main/java/backend/ContentTypeAndBodyMismatch.java
@@ -1,14 +1,13 @@
 package backend;
 
 import javax.net.ServerSocketFactory;
-import javax.net.ssl.SSLServerSocketFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ContentLengthHeaderDifferFromActualContentLength {
+public class ContentTypeAndBodyMismatch {
 
     public static void main(String[] args) {
         try {
@@ -57,11 +56,11 @@ public class ContentLengthHeaderDifferFromActualContentLength {
                 out.print("Access-Control-Allow-Origin: *\r\n");
                 out.print("X-Correlation-ID: 9f22c69b-6673-4326-8aff-0c0c097cd3c0\r\n");
                 out.print("Access-Control-Allow-Headers: authorization,Access-Control-Allow-Origin,Content-Type,SOAPAction,apikey,testKey,Authorization\r\n");
-                out.print("Content-Type: application/json\r\n");
+                out.print("Content-Type: text/xml\r\n");
                 out.print("Date: Tue, 14 Dec 2021 08:15:17 GMT\r\n");
                 //out.print("Transfer-Encoding: chunked\r\n");
-                //out.print("Content-Length:  " + line4.getBytes().length + "\r\n");; // The type of data
-                out.print("Content-Length: 36\r\n");; // The type of data
+                out.print("Content-Length:  " + line4.getBytes().length + "\r\n");; // The type of data
+                //out.print("Content-Length: 36\r\n");; // The type of data
                 //out.print("Content-Length: 10\r\n");; // The type of data
                 out.print("Connection: Close\r\n");
                 out.print("\r\n"); // End of headers
