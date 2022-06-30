05bb3cf5a79a98b40598340d717d6eb89d999ee8
Sajinie <sajiniekavindya@gmail.com>
2022-06-30 14:37:27 +0530

    Rename the class

--- src/main/java/backend/ClosesTheConnectionWhileBackendReadingRequestHeaders.java
+++ src/main/java/backend/ClosesTheConnectionWhileBackendReadingRequestHeaders.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ClosesTheConnectionWhileReadingRequestHeaders {
+public class ClosesTheConnectionWhileBackendReadingRequestHeaders {
 
     public static void main(String[] args) {
         try {
