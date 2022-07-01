5aeb4c38ef1b44b8a0ef869e9ee023b98b4b6393
Sajinie <sajiniekavindya@gmail.com>
2022-07-01 16:21:23 +0530

    Add case 14

---
+++ src/main/java/backend/case14/BackendClosesTheConnectionWhileMISendingTheRequestHeaders.java
@@ -0,0 +1,48 @@
+package backend.case14;
+
+import javax.net.ServerSocketFactory;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BackendClosesTheConnectionWhileMISendingTheRequestHeaders {
+
+    public static void main(String[] args) {
+        try {
+            // Create a ServerSocket to listen on that port.
+            ServerSocketFactory ssf = ServerSocketFactory.getDefault();
+            ServerSocket ss = ssf.createServerSocket(7005);
+            System.out.println("Server started");
+
+            // Now enter an infinite loop, waiting for & handling connections.
+            for (;;) {
+                // Wait for a client to connect. The method will block;
+                // when it returns the socket will be connected to the client
+                Socket clientSocket = ss.accept();
+                System.out.println("connection accepted");
+                clientSocket.setReceiveBufferSize(3);
+
+                // Get input and output streams to talk to the client
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                //code to read and print headers
+                int i = 0;
+                String headerLine = null;
+                while((headerLine = in.readLine()).length() != 0) {
+                    System.out.println(headerLine);
+                    clientSocket.close();
+                    System.out.println("closed the socket");
+                    break;
+                }
+                in.close();
+
+            } // Now loop again, waiting for the next connection
+        }
+        // If anything goes wrong, print an error message
+        catch (Exception e) {
+            System.err.println(e);
+            System.err.println("Usage: java HttpMirror <port>");
+        }
+    }
+}
---
+++ src/main/java/backend/case14/SlowWriteClient.java
@@ -0,0 +1,139 @@
+package client;
+
+import client.helpers.RequestMethods;
+import client.helpers.TestPayloads;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class SlowWrite {
+
+    private String host = "localhost";
+    private int port = 8290;
+    public static final String CRLF = "\r\n";
+
+    public static void main(String[] args) {
+
+        SlowWrite client = new SlowWrite();
+        client.run();
+    }
+
+    SlowWrite() {
+
+    }
+
+    SlowWrite(String host, int port) {
+
+        this.host = host;
+        this.port = port;
+    }
+
+    // Start to run the server
+    public void run() {
+
+        try {
+            // Create socket
+            Socket socket = new Socket(this.host, this.port);
+
+            System.out.println("client started");
+            new SlowWrite.ClientThread(socket).start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Thread handling the socket to server
+    static class ClientThread extends Thread {
+
+        private Socket socket = null;
+
+        ClientThread(Socket socket) {
+
+            this.socket = socket;
+        }
+
+        public void run() {
+
+            try {
+                // Start handling application content
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                PrintStream printWriter = new PrintStream(socket.getOutputStream());
+                // Write data
+
+                String payload = TestPayloads.FULL_PAYLOAD;
+                RequestMethods method = RequestMethods.POST;
+
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(method + " /test HTTP/1.1\r\n");
+//                sb.append("Accept: application/json\r\n");
+//                sb.append("Connection: keep-alive\r\n");
+//                printWriter
+//                        .print("Authorization: Bearer eyJ4NXQiOiJOVGRtWmpNNFpEazNOalkwWXpjNU1tWm1PRGd3TVRFM01XWXdOREU1TVdSbFpEZzROemM0WkEiLCJraWQiOiJNell4TW1Ga09HWXdNV0kwWldObU5EY3hOR1l3WW1NNFpUQTNNV0kyTkRBelpHUXpOR00wWkdSbE5qSmtPREZrWkRSaU9URmtNV0ZoTXpVMlpHVmxOZ19SUzI1NiIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJhZG1pbiIsImF1dCI6IkFQUExJQ0FUSU9OIiwiYXVkIjoiY1o4Q2xHUE9rblBkZFo1eEpNZl9CX2k3VFBRYSIsIm5iZiI6MTY1MjIxNjQ5NiwiYXpwIjoiY1o4Q2xHUE9rblBkZFo1eEpNZl9CX2k3VFBRYSIsInNjb3BlIjoiZGVmYXVsdCIsImlzcyI6Imh0dHBzOlwvXC9sb2NhbGhvc3Q6OTQ0M1wvb2F1dGgyXC90b2tlbiIsImV4cCI6MTY1MjIyMDA5NiwiaWF0IjoxNjUyMjE2NDk2LCJqdGkiOiI0ZjliYjkzNy1iNzU4LTQ5MWEtYTM0ZC00Y2MyOTI2OWFhNzIifQ.tru_XsD-kGj2-Eaxsij4f55kM21LsDTKE7voW7SGhcZ2EllVJJBZdL7y_L8Jwv1tMWbfm_i5iHgtrnLrXJY3zUItpOU6IT04oBrFhiI2n4AWC138TeZvJXmH8W2ZAz2vddGpHogtvUwP5Ga_DT43Rtnh0PyTXySOlZQvL6LjR0oiWqjJaIMuuIohsNgRhVdjN8AgSeF2pb_h9jVJStkcK5eIHoom2ZZQeqr0EgtkJgCnft-Z143_83_KUe3pyAU4pzYYhVMTjYPKXIVhx56Z-HSt7UHCe2f1cu_viAyff-LzNcfpyBfj2u5rzTiYlfLtnKVM8ilS7b8hmH307oXb4A\r\n");
+//                printWriter
+//                        .print("DB-ID: eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0d0Y2Ym8tVXRuTFg4VTVoSjNPUkVhQlVVcDB2SVpqMTVUU2NVNUpKcmU4In0.eyJqdGkiOiJlYzdkZGIxNC1hMGMxLTRhMzItYWFmNC05MDdlMDk4OTQxN2YiLCJleHAiOjE2NTA3MjQ3MzksIm5iZiI6MTY1MDY4ODczOSwiaWF0IjoxNjUwNjg4NzM5LCJpc3MiOiJodHRwczovL2VpZHAtdWF0LmRlLmRiLmNvbS9hdXRoL3JlYWxtcy9nbG9iYWwiLCJhdWQiOiIxMTUwOTUtMV9MZW5kaW5nU2VydmljZUxheWVyIiwic3ViIjoiZmI4OTZmMjEtYzQwZi00YjUzLWE4YWMtODVhZTRmNjcxMmJlIiwidHlwIjoiRWlkcC1BdXRoeiIsImF6cCI6IjExNTA5NS0xX0xlbmRpbmdTZXJ2aWNlTGF5ZXIiLCJuYXJpZCI6IjExNTA5NS0xIiwibGVnaXRpbWF0aW9ucyI6W3siaWQiOiJBMjMwMDgiLCJndm8iOlsiUEE3L1BBUlROIiwiUEE3L1NFQSJdLCJsZ19uYXJpZCI6IjExNTA5NS0xIiwibGVnaV9hdXRoIjpbImxnOlBBNy9QQVJUTiIsImxnOlBBNy9TRUEiXSwiaWF0IjoxNjUwNjg4NzM5fV0sImRibGVnaWlkIjoiQTIzMDA4IiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYTIzMDA4IiwibWVtYmVyc2hpcHMiOltdfQ.TbU0ygbWdXQWtegC-tF7Dzl6uYECLSL1mMHQ43ls74g29W4SlAMQcruQVcydF69mSd0vbruTaRvrEG7CwyAIlFF8cYbRs62eQ6BDIim6WhFa0tOmLPRZ63gNGyVcpCbQisXjtzeFDYO6bq0eToTY_dntMkp6lsMXmgwOCVGXg1yopQnsl7XqrfRkZbwukeWBTQ3lbJYIkEIjqrDC1nU1fr9qwN6r2ntp71dGnqsiy6sZRQvlCKLlZSZ_NfWGuz4s-yxd9DFhIcSsvfSUhTuSZThJfw3_CCOSBTWB6Q4r0O9lHetwjI2h6-7DX2WZK_zl61nem1h1rd-EkcIjVU7uxg\r\n");
+//                if (!method.equals(RequestMethods.GET)) {
+//                    sb.append("Content-Type: application/json\r\n");
+//                    sb.append("Content-Length: " + payload.length() + "\r\n");
+//                }
+
+                printWriter.print(method + " /test HTTP/1.1\r\n");
+                printWriter.print("Accept: application/json\r\n");
+                printWriter.print("Connection: keep-alive\r\n");
+                printWriter.print("Transfer-Encoding: chunked\n");
+                printWriter.print("Content-Type: text/plain\r\n");
+                printWriter.print("\r\n");
+//                printWriter.print("Content-Length: " + payload.length() + "\r\n");
+
+                printWriter.flush();
+//                sb.append("\r\n");
+//                if (!method.equals(RequestMethods.GET)) {
+//                sb.append(payload);
+//////                }
+//                for (int i = 0; i < sb.length(); ++i) {
+//                    printWriter.print(sb.charAt(i));
+//                    System.out.print(sb.charAt(i));
+//                    if (i%30==0) {
+//                        printWriter.printf("%x" + "\r\n");
+//                        Thread.sleep(50);
+//                    }
+//                    printWriter.flush();
+//                }
+
+                InputStream stream = new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8));
+
+                int chunkSize = 50;
+                int count;
+                byte[] buffer = new byte[chunkSize];
+
+                while ((count = stream.read(buffer)) > 0) {
+                    printWriter.printf("%x" + "\r\n", count);
+                    printWriter.write(buffer, 0, count);
+                    printWriter.print(CRLF);
+                    Thread.sleep(50);
+                }
+
+                printWriter.print("0" + CRLF);
+                printWriter.print(CRLF);
+                printWriter.flush();
+
+                String line = null;
+                int i = 0;
+                while ((line = bufferedReader.readLine()) != null) {
+                    i++;
+                    System.out.println("Inut : " + line);
+                    if (line.equals("0")) {
+                        break;
+                    }
+                }
+                printWriter.close();
+                bufferedReader.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
